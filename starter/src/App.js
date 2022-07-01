2bc94d2673c7a3e4837cb9face729492db154ab6
saifmohamedsv <xcyberghostx1@gmail.com>
2022-07-01 18:55:44 +0200

    22

--- starter/src/App.js
+++ starter/src/App.js
@@ -3,6 +3,7 @@ import CartContainer from "./components/CartContainer";
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {calculateTotals} from "./redux/reducers/cartReducer";
+import Modal from "./components/Modal";
 
 function App() {
     const {cartItems} = useSelector(state => state.cart)
@@ -14,6 +15,7 @@ function App() {
 
     return (
         <main>
+            <Modal/>
             <Navbar/>
             <CartContainer/>
         </main>
--- starter/src/redux/store.js
+++ starter/src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartSlice from "./reducers/cartReducer";
+import modalSlice from "./reducers/modalReducer";
 
 export const store = configureStore({
     reducer: {
-        cart: cartSlice
+        cart: cartSlice,
+        modal: modalSlice
     }
 })
